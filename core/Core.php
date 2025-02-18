c2b89da38a3685e766387cdd0efcabb8ff8f1962
Lucas Correia <lucasdevproject@gmail.com>
2025-02-18 09:04:43 -0300

    Sistema de Rotas Criado

--- core/Core.php
+++ core/Core.php
@@ -12,16 +12,32 @@ class Core
       $url .= ''; // Não é necessário, pois não altera nada
     }
 
+    ($url != '/') ? $url = rtrim($url, '/') : $url;
+
+    $routerFound = false;
+
     foreach ($routes as $path => $controller) {
-      $pattern = '#^' . preg_replace('/{id}/', '([\w+])', $path) . '$#';
+      $pattern = '#^' . preg_replace('/{id}/', '(\w+)', $path) . '$#';
 
       if (preg_match($pattern, $url, $matches)) {
         array_shift($matches);
-        print_r($matches);
+
+        $routerFound = true;
+
+
         [$currentController, $action] = explode('@', $controller);
 
-        require_once __DIR__ . "/../controller/$currentController.php";
+
+        require_once __DIR__ . "/../src/controllers/$currentController.php";
+
+        $newController = new $currentController();
+        $newController->$action($matches);
       }
+    }
+    if (!$routerFound) {
+      require_once __DIR__ . "/../src/controllers/NotFoundController.php";
+      $controller = new NotFoundController();
+      $controller->index();
     }
   }
 }

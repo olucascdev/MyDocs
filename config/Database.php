59472ef56e1f753dc79a43257001e6c30bf39434
olucascdev <lucasdevproject@gmail.com>
2025-03-13 13:08:25 -0300

    🪛 Modificado para melhor segurança de informações

--- config/Database.php
+++ config/Database.php
@@ -1,9 +1,17 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+// Carregar as variáveis do arquivo .env
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 // Configurações de conexão com o banco de dados
-$host = 'localhost';
-$dbname = 'mydocs';
-$username = 'root';
-$password = '';
+$host = $_ENV['DB_HOST'];
+$dbname = $_ENV['DB_NAME'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
 
 try {
     // Criando a conexão com o banco de dados usando PDO
@@ -21,4 +29,3 @@ try {
 }
 
 // Não use return, apenas deixe a variável $pdo disponível
-?>
